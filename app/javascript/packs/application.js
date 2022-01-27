abc6d0bb807cd5aa69656824e7260b4385cccdb8
Kyri Panayi <kpanayi7@gmail.com>
2022-01-27 22:23:40 +0000

    amended javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ require("channels")
 import 'bootstrap';
 import 'select2/dist/css/select2.min.css'
 import 'jquery-bar-rating/dist/themes/css-stars';
-import 'aos/dist/aos.css';
-import AOS from 'aos';
 import { initTyped } from './plugins/init_typedjs';
 import { initSelect2 } from './plugins/init_select2';
 import { initStarRating } from './plugins/init_star_rating';
@@ -21,6 +19,5 @@ import { initStarRating } from './plugins/init_star_rating';
 document.addEventListener('turbolinks:load', () => {
   initSelect2();
   initTyped();
-  AOS.init();
   initStarRating();
 });
